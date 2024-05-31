15722c5826915d9b9e0213a818db971b3400abd9
Muki Dev <mukidev881@gmail.com>
2024-05-31 04:12:30 +0200

    add event

--- src/App.js
+++ src/App.js
@@ -8,15 +8,31 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      input: ''
+    }
+  }
 
+  onInputChange =  (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render(){
     return(
       <div className='App'>
-          <ParticlesBg type="circle" bg={true} />
+          <ParticlesBg type="fountains" bg={true} />
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+          />
       </div>
        
       )
