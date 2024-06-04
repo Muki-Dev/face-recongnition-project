d46f074269519002adb84fd04d0e92b071450fe4
Muki Dev <mukidev881@gmail.com>
2024-06-04 15:52:25 +0200

    connect facerecognition to imagefromlink

--- src/App.js
+++ src/App.js
@@ -18,26 +18,25 @@ class App extends Component{
   constructor(){
     super()
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange =  (event) => {
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-
+    this.setState({imageUrl: this.state.input})
     app.models.predict(
-      '53e1df302c079b3db8a0a36033ed2d15',
-      'https://samples.clarifai.com/metro-north.jpg')
+   Clarifai.FACE_DETECT_MODEL, this.state.input)
     .then(
         function(response){
-            console.log(response);
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function(err){
-          console.log(err);
+          
         }
       );
 
@@ -53,7 +52,7 @@ class App extends Component{
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit} 
           />
-          <FaceRecognition />
+          <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
        
       )
